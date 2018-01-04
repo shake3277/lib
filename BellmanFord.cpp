1e18a28bb102a53e76d0f49e078118404c285de5
shake3277 <syageen@gmail.com>
2018-01-04 22:10:30 +0900

    add

---
+++ BellmanFord.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX_V 10000000
+#define MAX_E 10000000
+#define INF 1111111111
+struct edge{int from,to,cost;};
+
+edge es[MAX_E]; //辺
+int d[MAX_V]; //最短距離
+int V,E; //頂点数、辺数
+
+//最短距離を求める
+void shortest_path(int s){
+  fill(d,d+V,INF);
+  d[s]=0;
+  while(1){
+    bool update=false;
+    for(int i=0;i<E;i++){
+      edge e=es[i];
+      if(d[e.from]!=INF&&d[e.to]>d[e.from]+e.cost){
+	d[e.to]=d[e.from]+e.cost;
+	update=true;
+      }
+    }
+    if(!update)break;
+  }
+}
+
+//閉路があるか?
+bool find_negative_loop(){
+  memset(d,0,sizeof(d));
+  for(int i=0;i<V;i++){
+    for(int j=0;j<E;j++){
+      edge e=es[j];
+      if(d[e.to]>d[e.from]+e.cost){
+	d[e.to]=d[e.from]+e.cost;
+	if(i==V-1)return true;
+      }
+    }
+  }  
+  return false;
+}
