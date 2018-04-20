85ccf3f58d68f9fd293d3b23daf72d81ec77fdf3
shake3277 <syageen@gmail.com>
2018-04-20 16:40:16 +0900

    add

--- WarshallFloyd.cpp
+++ WarshallFloyd.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 #define MAX_V 10000
 #define INF 2000000001
-int d[MAX_V][MAX_V]; //d[u][v]はe=(u,v)のコスト(存在しなければINF、d[i][i]は0)
+int d[MAX_V][MAX_V]; //d[u][v]はe=(u,v)のコスト(存在しなければINF、u=vは0)
 int V; //頂点数
+
+void init(){
+  for(int i=0;i<V;i++)
+    for(int j=0;j<V;j++)
+      if(i==j)d[i][j]=0;
+      else d[i][j]=INF;
+}
 
 void warshall_floyd(){
   for(int k=0;k<V;k++)
