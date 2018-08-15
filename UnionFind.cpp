30268a5fa150b028e6d82441c9dd313a27dc5ee5
shake <shake@shakenoMacBook-puro.local>
2018-08-15 22:26:08 +0900

    fix

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,37 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX_N 100000
 
-int par[MAX_N]; //親
-int ran[MAX_N]; //木の深さ
-
-//初期化
-void init(int n){
-  for(int i=0;i<n;i++){
-    par[i]=i;
-    ran[i]=0;
+struct UnionFind{
+  int n;
+  vector<int> p,r;
+  UnionFind(){}
+  UnionFind(int sz):n(sz),r(sz,1),p(sz,0){iota(p.begin(),p.end(),0);}
+  int find(int x){
+    if(p[x]==x)return x;
+    else return p[x]=find(p[x]);
   }
-}
-
-//根を求める
-int find(int x){
-  if(par[x]==x)return x;
-  else return par[x]=find(par[x]);
-}
-
-//集合を合併
-void unite(int x,int y){
-  x=find(x);
-  y=find(y);
-  if(ran[x]<ran[y])par[x]=y;
-  else {
-    par[y]=x;
-    if(ran[x]==ran[y])ran[x]++;
-
+  void unite(int x,int y){
+    x=find(x);
+    y=find(y);
+    if(r[x]<r[y])p[x]=y;
+    else {
+      p[y]=x;
+      if(r[x]==r[y])r[x]++;
+    }
   }
-}
+  bool same(int x,int y){
+    return find(x)==find(y);
+  }
+};
 
-//xとyが同じ集合に属するのか?
-bool same(int x,int y){
-  return find(x)==find(y);
+int main(){
+  int n,q;
+  cin>>n>>q;
+  UnionFind uf(n);
+  for(int i=0;i<q;i++){
+    int c,x,y;
+    cin>>c>>x>>y;
+    if(c)cout<<uf.same(x,y)<<endl;
+    else uf.unite(x,y);
+  }
+  return 0;
 }
