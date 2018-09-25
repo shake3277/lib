7388d3dc38838a3c248c009b2f09f4cd85f5f4f8
shake3277 <syageen@gmail.com>
2018-09-25 16:46:34 +0900

    add

--- mod_pow.cpp
+++ mod_pow.cpp
@@ -8,29 +8,35 @@ const ll N = 1e6;
 //(x^n)%mod
 ll mod_pow(ll x,ll n,ll mod){
   if(n==0)return 1;
-  ll res=mod_pow(x*x%mod,n/2,mod);
-  if(n&1)res=res*x%mod;
+  ll res=mod_pow( x*x%mod , n/2 , mod );
+  if( n & 1 )res = res*x%mod;
   return res;
 }
 
-ll rev[N];
-ll inr[N];
+ll fact[N];
+ll infact[N];
 
-void make_comb(void){
-  rev[0]=1;
-  for(ll i=1;i<N;i++){
-    rev[i]=rev[i-1]*i;
-    rev[i]%=MOD;
+void make_factorial(void){
+  fact[0] = 1;
+  for( ll i = 1 ; i < N ; i++ ){
+    fact[i] = fact[i-1] * i;
+    fact[i] %= MOD;
   }
-  for(ll i=0;i<N;i++){
-    inr[i]=mod_pow(rev[i],MOD-2,MOD);
+  for( ll i = 0 ; i < N ; i++ ){
+    infact[i] = mod_pow( fact[i] , MOD-2 , MOD );
   }
 }
 
 ll comb(ll a,ll b){
   if(a == 0 && b == 0)return 1;
   if(a < b || a < 0)return 0;
-  ll tmp = inr[a-b]* inr[b] % MOD;
-  return tmp * rev[a] % MOD; 
+  ll tmp = infact[a-b]* infact[b] % MOD;
+  return tmp * fact[a] % MOD; 
+}
+
+ll perm(ll a,ll b){
+  if(a == 0 && b == 0)return 1;
+  if(a < b || a < 0)return 0;
+  return fact[a] * infact[a-b] % MOD;
 }
 
