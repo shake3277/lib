bf0760e13b0ef7449bf2d313611a9ca657f2687b
shake3277 <syageen@gmail.com>
2019-01-08 12:57:47 +0900

    add

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,19 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX_N 100000
-int prime[MAX_N];//i番目の素数
-bool is_prime[MAX_N+1];
 
-//n以下の素数の数を返す
-int sieve(int n){
-  int p=0;
-  for(int i=0;i<=n;i++)is_prime[i]=true;
-  is_prime[0]=is_prime[1]=false;
-  for(int i=2;i<=n;i++){
-    if(is_prime[i]){
-      prime[p++]=i;
-      for(int j=2*i;j<=n;j+=i)is_prime[j]=false;
+struct Sieve{
+  int prime[MAX_N]={};//i番目の素数
+  bool is_prime[MAX_N+1]={};
+  
+  //n以下の素数の数を返す
+  int sieve(int n){
+    int p=0;
+    for(int i=0;i<=n;i++)is_prime[i]=true;
+    is_prime[0]=is_prime[1]=false;
+    for(int i=2;i<=n;i++){
+	 if(is_prime[i]){
+	   prime[p++]=i;
+	   for(int j=2*i;j<=n;j+=i)is_prime[j]=false;
+	 }
     }
+    return p;
   }
-  return p;
-}
+};
