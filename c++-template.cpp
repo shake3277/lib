56657ee90d3de504a2fd7416347fdc9c29e9eb3f
shake3277 <syageen@gmail.com>
2019-01-08 12:59:30 +0900

    add

---
+++ c++-template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll,ll> pl;
+typedef pair<int,int> pi;
+typedef pair<ll,pl> pp;
+const ll MOD = 1e9+7;
+const ll MAX = 1e18*2;
+
+int main(){
+  
+}
