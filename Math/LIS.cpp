dd471571529d4e0c8073b460e4ba60e9ec3abf33
shake <syageen@gmail.com>
2022-03-10 23:54:06 +0900

    構造体に変更

--- Math/LIS.cpp
+++ Math/LIS.cpp
@@ -1,21 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX_N 111111
-int n,a[MAX_N],dp[MAX_N];
 
-int Lis(){
-  dp[0]=a[0];
-  int ans=1;
-  for(int i=1;i<n;i++){
-    if(dp[ans-1]<a[i])dp[ans++]=a[i];
-    else *lower_bound(dp,dp+ans,a[i])=a[i];
+struct LIS{
+  int n;
+  vector<int> a,dp;
+  LIS(){}
+  LIS(int _n,vector<int> _a):n(_n),a(_a),dp(sz,0):{}
+
+  int res(){
+    dp[0]=a[0];
+    int ans=1;
+    for(int i=1;i<n;i++){
+      if(dp[ans-1]<a[i])dp[ans++]=a[i];
+      else *lower_bound(dp,dp+ans,a[i])=a[i];
+    }
+    return ans;
   }
-  return ans;
 }
 
 int main(){
+  int n;
   cin>>n;
+  vector<int> a(n);  
   for(int i=0;i<n;i++)cin>>a[i];
-  cout<<Lis()<<endl;
+  LIS lis(n,a);
+  cout<<lis.res()<<endl;
   return 0;
 }
